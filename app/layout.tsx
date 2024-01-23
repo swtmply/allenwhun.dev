27a277f43fcaf77eb4d9d9989f157bef5996fc72
swtmply <delosreyesjohnallen@gmail.com>
2024-01-23 16:26:35 +0800

    style: changed the default font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans, DM_Mono as FontMono } from "next/font/google";
 import "./globals.css";
+import { cn } from "~/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontMono.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
